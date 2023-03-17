dfadeda79c55dc7b43a44e2cfe6edff95ba1788c
Zuha Khundmiri <70804630+Zuha-2109@users.noreply.github.com>
2023-03-17 20:55:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-// yourArray = [1, "hello", true, "Zuha", 24];
-yourArray = ['zuha' , 1 , true , 'four' , 4 , null , "zuha"];
+
+const yourArray = ['zuha' , 1 , true , 'four' , 4 , null , "zuha"];
+
